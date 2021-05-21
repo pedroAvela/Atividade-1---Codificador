80b0cec5facce98719f1f2866440673f83420d7e
Pedro Henrique Avelar Borges <>
2021-05-21 05:40:16 +0000

    O programa já troca as letras

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+void codificador(string *word, char letter){
+  string old_word = *word;
+  for(int i = 0; i < old_word.length(); i++){
+    if (old_word[i] == letter){
+      old_word[i] = 'x';
+    }
+  }
+
+  *word = old_word;
+
+  return;
+  
+}
+
 int main() {
-  cout << "Hello World!\n";
+  string palavra = "internet", *p;
+  int n = palavra.length();
+  char letra = 'e';
+  char vetor[n];
+
+  p = &palavra;
+
+  cout << palavra << endl;
+
+  codificador(p, letra);
+ 
+  cout << palavra;
+  
 }
